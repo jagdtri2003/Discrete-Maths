f745724cc7c42021f836b186c9d0e8f1ea097bb5
Jagdamba Tripathi <54079135+jagdtri2003@users.noreply.github.com>
2022-10-19 11:18:05 +0530

    Create powerSet.c

---
+++ powerSet.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main(){
+
+	int n1,i,j,Psize=1;
+	printf("Enter the number of element in Set A : ");
+	scanf("%d",&n1);
+	
+	//Calculating The Size Of Power Set 
+
+	for (i=0;i<n1;i++){
+		Psize*=2;
+	}
+	int A[n1],P[Psize];
+	for(i=0;i<n1;i++){
+		printf("Enter the element %d of Set A : ",i+1);
+		scanf("%d",&A[i]);
+	}
+	
+	printf("Power Set of A is : { ");
+
+	for (i=0;i<Psize;i++){
+		printf("{ ");
+		for (j=0;j<n1;j++){
+		
+			if (i & (1<<j)){
+				printf("%d,",A[j]);
+			}		
+		}
+		printf("\b },");
+
+	}
+	printf("\b }\n");
+
+	return 0;
+}
