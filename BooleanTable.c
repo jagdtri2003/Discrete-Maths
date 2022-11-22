0ea4136ed068f6d3b2ad41ea1ba189135aa67118
Jagdamba Tripathi <jagdtri2003@gmail.com>
2022-11-22 19:57:24 +0530

    used 2D matrix

--- BooleanTable.c
+++ BooleanTable.c
@@ -4,22 +4,21 @@ int main(){
 
 	
 	int i,j,x,A,B;
+	int AND[2][2],OR[2][2];
 	printf("Truth Table of AND : \n");
 	printf("A\tB\tA and B\n");
 	for (i=0;i<2;i++){
-
 		for (j=0;j<2;j++){
-
-			printf("%d\t%d\t %d\n",i,j,i&&j);
+			AND[i][j]=i&&j;
+			printf("%d\t%d\t %d\n",i,j,AND[i][j]);
 		}
 	}
 	printf("Truth Table of OR : \n");
 	printf("A\tB\tA or B\n");
 	for (i=0;i<2;i++){
-
 		for (j=0;j<2;j++){
-
-			printf("%d\t%d\t %d\n",i,j,i||j);
+			OR[i][j]=i||j;
+			printf("%d\t%d\t %d\n",i,j,OR[i][j]);
 		}
 	}
 	printf("Truth Table of NOT : \n");
